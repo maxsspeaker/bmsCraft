813e8f894595f4952c2d5e2cbf18b996f539b68c
maxsspeaker <56259377+maxsspeaker@users.noreply.github.com>
2021-01-22 11:14:16 +0300

    новая версия американской локали

--- bmsCraft/en.py
+++ bmsCraft/en.py
@@ -1,7 +1,14 @@
-battanplaygame1_1="new game"
-loading_game="loading game"
+battanplaygame1_1="start the game"
+
+battanLoadGame="load the world"
+battanCreatGame="new world"
+battanplaygameONLAIN1_1="online game*"
+loading_game="loading the game"
 version_name="version: "
 
 exit_battanmenu="exit"
 
 setingsbottan="setings"
+ventutsaBattan="back"
+
+localization="language"
